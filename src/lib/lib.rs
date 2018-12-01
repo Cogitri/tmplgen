a0f5a493fb79c9c6b5f9176196cca67a4bddba72
Rasmus Thomsen <Rasmus.thomsen@protonmail.com>
2018-12-01 22:55:20 +0100

    lib: add top page to docs

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -13,10 +13,37 @@
 //You should have received a copy of the GNU General Public License
 //along with tmplgen.  If not, see <http://www.gnu.org/licenses/>.
 
-pub mod crates;
-pub mod gems;
+//! libtmplgen can be used for querying different language-specific package managers and generating
+//! Void Linux build templates for them. Currently the following providers are supported:
+//!
+//! * [crates.io](https://crates.io)
+//! * [metacpan.org](https://metacpan.org)
+//! * [rubygems.org](https://rubygems.org)
+//!
+//! # Usage
+//!
+//! The following will write a template for `tmplgen` in $XBPS_DISTDIR/srcpkgs/tmplgen/template
+//!
+//! ```
+//! use libtmplgen::*;
+//!
+//! // Get the PkgType of this crate
+//! let pkg_type = figure_out_provider("tmplgen").unwrap();
+//! // Get a PkgInfo struct of this crate
+//! let pkg_info = get_pkginfo("tmplgen", &pkg_type).unwrap();
+//! // Don't overwrite existing templates
+//! let force_overwrite = false;
+//! // This isn't a recursive dep, error out if there's an error
+//! let is_rec = false;
+//!
+//! template_handler(&pkg_info, &pkg_type, force_overwrite, is_rec);
+//! ```
+
+mod crates;
+mod gems;
+mod perldist;
+
 pub mod helpers;
-pub mod perldist;
 pub mod tmplwriter;
 pub mod types;
 #[cfg(test)]
