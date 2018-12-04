6d7350b599cc072f8d2f52a675bff58cda745105
Rasmus Thomsen <Rasmus.thomsen@protonmail.com>
2018-12-04 10:23:16 +0100

    bin-tests: add test_bin_update

--- src/bin/tests/mod.rs
+++ src/bin/tests/mod.rs
@@ -3,12 +3,39 @@ use std::process::Command;
 use tempfile::tempdir;
 
 #[test]
-fn test_bin_run() {
+fn test_bin_gen() {
     let dir = tempdir().unwrap();
 
     Command::main_binary()
         .unwrap()
         .args(&["tmplgen"])
+        .env_clear()
+        .env("XBPS_DISTDIR", dir.path().join("tmplgen-tests/"))
+        .env("GIT_AUTHOR_NAME", "tmplgentests")
+        .env("GIT_AUTHOR_EMAIL", "tmplgen@tests.de")
+        .assert()
+        .success();
+
+    dir.close().unwrap();
+}
+
+#[test]
+fn test_bin_update() {
+    let dir = tempdir().unwrap();
+
+    Command::main_binary()
+        .unwrap()
+        .args(&["tmplgen"])
+        .env_clear()
+        .env("XBPS_DISTDIR", dir.path().join("tmplgen-tests/"))
+        .env("GIT_AUTHOR_NAME", "tmplgentests")
+        .env("GIT_AUTHOR_EMAIL", "tmplgen@tests.de")
+        .assert()
+        .success();
+
+    Command::main_binary()
+        .unwrap()
+        .args(&["-U", "tmplgen"])
         .env_clear()
         .env("XBPS_DISTDIR", dir.path().join("tmplgen-tests/"))
         .env("GIT_AUTHOR_NAME", "tmplgentests")
