f9284637e8dae1a9129f93f1e36e83ae292e414d
Rasmus Thomsen <Rasmus.thomsen@protonmail.com>
2019-03-03 17:03:42 +0100

    chore(deps): update crates_io_api 0.3.0 -> 0.4.0
    This also allows us setting a user agent for crates.io

--- src/lib/crates.rs
+++ src/lib/crates.rs
@@ -29,8 +29,7 @@ use retry::retry_exponentially;
 // TODO: Switch to AsyncClient
 pub(super) fn crate_info(crate_name: &str) -> Result<PkgInfo, Error> {
     let client =
-        //crates_io_api::SyncClient::with_user_agent("tmplgen/1 (github.com/Cogitri/tmplgen)");
-        crates_io_api::SyncClient::new();
+        crates_io_api::SyncClient::with_user_agent(&format!("tmplgen/{} (github.com/Cogitri/tmplgen)", env!("CARGO_PKG_VERSION")));
 
     let query_result = match retry_exponentially(
         3,
